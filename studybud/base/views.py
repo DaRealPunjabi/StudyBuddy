42f059b354e30396eb5493f0939331530bdd5b3b
DaRealPunjabi <shiv.jalli@gmail.com>
2023-12-30 16:06:31 +0000

    Template dyn url 1

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -14,6 +14,12 @@ def home(request):
     return render(request, 'base/home.html', context)
 #    return HttpResponse('Home Page')
 
-def room(request):
-    return render(request, 'base/room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}        
+    return render(request, 'base/room.html', context)
 #    return HttpResponse('Room')
+
