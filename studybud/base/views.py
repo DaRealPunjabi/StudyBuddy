925ca0aaff7e3934d69f288e467999e7d1c8b4ea
DaRealPunjabi <shiv.jalli@gmail.com>
2023-12-30 15:37:45 +0000

    Template main and app

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,16 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 rooms = [
-    {'id':1, 'name':'Lets learn python!'},
+    {'id':1, 'name':'Lets learn python1!'},
     {'id':2, 'name':'Design with me'},
     {'id':3, 'name':'Frontend developers'},
 ]
 
 def home(request):
     context = {'rooms': rooms}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
 #    return HttpResponse('Home Page')
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
 #    return HttpResponse('Room')
