fdf3eb2e63af6df15512e3a28618d3769b0f474c
DaRealPunjabi <shiv.jalli@gmail.com>
2023-12-31 09:14:06 +0000

    Restricted Pages

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import is_valid_path
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic, Message, User
@@ -16,7 +17,6 @@ rooms = [
 ]
 
 def loginPage(request):
-    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
@@ -76,6 +76,7 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 #    return HttpResponse('Room')
 
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm
 
@@ -89,9 +90,13 @@ def createRoom(request):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here')
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
@@ -102,9 +107,12 @@ def updateRoom(request, pk):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
-
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here')
+    
     if request.method == 'POST':
         room.delete()
         return redirect('home')
