a44dd7b8c3ceb37cd38457bc64085c47d92bf70b
DaRealPunjabi <shiv.jalli@gmail.com>
2023-12-31 07:54:12 +0000

    User Login Flash Messages

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import is_valid_path
+from django.contrib import messages
 from django.db.models import Q
-from .models import Room, Topic
+from django.contrib.auth import authenticate, login
+from .models import Room, Topic, Message, User
 from .forms import RoomForm
 
 # Create your views here.
@@ -12,6 +14,33 @@ rooms = [
     {'id':2, 'name':'Design with me'},
     {'id':3, 'name':'Frontend developers'},
 ]
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        user_name = request.POST.get('uname')
+        user_pwd = request.POST.get('pwd')
+
+        try:
+            user = User.objects.get(username=user_name)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=user_name, password=user_pwd)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exit') 
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
