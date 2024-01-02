1fc39f178b7225f394a94ae0a87577dc5a86363e
DaRealPunjabi <shiv.jalli@gmail.com>
2024-01-02 10:50:54 +0000

    Activity Feed

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -84,7 +84,12 @@ def home(request):
 
     topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics, 'room_count':room_count}
+    # room_messages = Message.objects.all().order_by('-created')
+
+    room_messages = Message.objects.filter(
+        Q(room__topic__name__icontains=q))[0:3]
+    context = {'rooms': rooms, 'topics': topics, 
+               'room_count':room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
 #    return HttpResponse('Home Page')
 
